83c1325c558afeeeabcb62891ad36bbbdb7bcbcd
Christoffer Fuglkjær <118052934+Christofferfuglkjaer@users.noreply.github.com>
2024-06-13 14:10:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,7 @@ import zipfile
 # Page title
 st.set_page_config(page_title='ML Model Building', page_icon='🤖')
 st.title('🤖 ML Model Building')
-
-with st.expander('About this app'):
-
-# Sidebar for accepting input parameters
-with st.sidebar:
-    # Load data
-    st.header('1.1. Input data')
-
-    st.markdown('**1. Use custom data**')
-    uploaded_file = st.file_uploader("Upload a txt file", type=["txt"])
-    if uploaded_file is not None:
-        df = pd.read_fwf(uploaded_file)
-    st.write(df)
+uploaded_file = st.file_uploader("Upload a txt file", type=["txt"])
+if uploaded_file is not None:
+    df = pd.read_fwf(uploaded_file)
+st.write(df)

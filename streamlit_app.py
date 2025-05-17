2a00bb6032ea1c5e93be81860e22324bfaa41cd0
Christoffer Fuglkjær <>
2025-05-17 19:03:41 +0200

    zxczxc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,6 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-import altair as alt
-import time
-import zipfile
+
 
 # Page title
 st.set_page_config(page_title='ML Model Building', page_icon='🤖')
-st.title('🤖 ML Model Building')
-uploaded_file = st.file_uploader("Upload a txt file", type=["txt"])
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-for d in df:
-    st.write(d)
-    st.write(df)
 
-for i in range(len(df)):
-
-    st.write(i)

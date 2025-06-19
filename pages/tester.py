83f6968c6a1abbc405d83f384483cecfeaa1f9ca
Christoffer Fuglkjær <>
2025-06-19 22:33:22 +0200

    asdfsdf

--- pages/tester.py
+++ pages/tester.py
@@ -3,30 +3,23 @@ import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Define the required scopes
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets",
 ]
 
-# Load credentials from Streamlit secrets
 skey = st.secrets["gcp_service_account"]
+st.write(skey)
 credentials = Credentials.from_service_account_info(
     skey,
     scopes=scopes,
 )
 client = gspread.authorize(credentials)
 
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
-def load_data(sheet_url, sheet_name="Ark1"):
-    sh = client.open_by_url(sheet_url)
-    worksheet = sh.worksheet(sheet_name)
-    data = worksheet.get_all_records()
-    df = pd.DataFrame(data)
+def load_data(url, sheet_name="Ark1"):
+    sh = client.open_by_url(url)
+    df = pd.DataFrame(sh.worksheet(sheet_name).get_all_records())
     return df
-
-# Example usage:
-sheet_url = "https://docs.google.com/spreadsheets/d/1kw4gzt-Ybzkl2QVk9nU04yvCTBsJBqP6t7WYklwzSxg/edit?gid=0#gid=0"  # Replace with your actual Google Sheet URL
-sheet_name = "Ark1"   # Replace with your worksheet name if different
-
-df = load_data(sheet_url, sheet_name)
-st.dataframe(df)
